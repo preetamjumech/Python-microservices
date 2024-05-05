e6c889015ebc16ebfc74a18d92c1be180aa48624
Preetam Saha <82079602+preetamjumech@users.noreply.github.com>
2024-05-05 15:31:01 +0000

    adding a new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,8 @@ def wiki(name="Indian Institute of Science", length=1):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+
+def search_wiki(name):
+    results = wikipedia.search(name)
+    return results
